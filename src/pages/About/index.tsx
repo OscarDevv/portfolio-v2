cb5e3820572f0b808c3a625dd1586f76fd93b614
unknown <oscanero3@gmail.com>
2026-06-15 18:01:50 -0300

    feat: coloca imagem na pagina sobre

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,5 +1,28 @@
+import pfp from "../../assets/images/Foto-Perfil.jpeg";
+import styles from "./About.module.scss";
+
 export const About = () => {
   return (
-    <p>About</p>
-  )
-}
+    <>
+      <div className={styles.container}>
+        <section>
+          <a
+            href="https://github.com/oscardevv"
+            target="_blank"
+            title="Navegar para a página do GitHub do Oscar"
+          >
+            <img className={styles.pfp} src={pfp} alt="Imagem do " />
+          </a>
+        </section>
+        <section className={styles.content}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam,
+            nobis sed odit exercitationem dolorem atque ipsum? Adipisci sapiente
+            repellat consectetur voluptatem. Consectetur distinctio itaque
+            molestias sit reiciendis. Unde, nesciunt suscipit?
+          </p>
+        </section>
+      </div>
+    </>
+  );
+};
