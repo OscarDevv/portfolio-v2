2d5d16225d83170d35b0e19f88d2a29a3147db5f
unknown <oscanero3@gmail.com>
2026-07-11 13:44:30 -0300

    feat: finaliza pagina de contato

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,3 +1,62 @@
+import { Mail } from "lucide-react";
+import { useWriteText } from "../../hooks/useWriteText";
+import type { ContactWay } from "./Contact.types";
+import { Container } from "../../components/Container";
+import { FaDiscord, FaGithub } from "react-icons/fa";
+import { Button } from "../../components/Button";
+import styles from "./Contact.module.scss";
+
 export default function Contact() {
-  return <p>Contact</p>;
+  const contactTitle = useWriteText("Contato", 75);
+  const contactWays: ContactWay[] = [
+    {
+      name: "E-mail",
+      icon: Mail,
+      content: "oscanero3@gmail.com",
+      link: "https://mail.google.com/mail/?view=cm&fs=1&to=oscanero3@gmail.com",
+    },
+    {
+      name: "GitHub",
+      icon: FaGithub,
+      content: "OscarDevv",
+      link: "https://github.com/OscarDevv",
+    },
+    {
+      name: "Discord",
+      icon: FaDiscord,
+      content: "oscar_devv",
+    },
+  ];
+
+  return (
+    <>
+      <h1 className="pageTitle">{contactTitle}</h1>
+
+      <div className={styles.container}>
+        {contactWays.map((way) => {
+          const Icon = way.icon;
+
+          return (
+            <Container key={way.name}>
+              <Icon className={styles.icon} />
+              <h2>{way.name}</h2>
+              <span>{way.content}</span>
+              {way.link && (
+                <a href={way.link} target="_blank">
+                  <Button>Conversar</Button>
+                </a>
+              )}
+              {!way.link && (
+                <p className={styles.noLink}>
+                  <strong>Sem link para conversar</strong>
+                </p>
+              )}
+            </Container>
+          );
+        })}
+      </div>
+
+      <p>Novas formas de conversar comigo serão adicionadas em breve.</p>
+    </>
+  );
 }
